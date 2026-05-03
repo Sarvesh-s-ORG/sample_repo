d6dcead80a32e6f9317d90670e22cd6d3c541542
sarrvessh <pvsarvesh29@gmail.com>
2026-05-04 02:47:14 +0530

    fix: correct return statement in read_log_file function

--- utils.py
+++ utils.py
@@ -11,5 +11,5 @@ def read_log_file(filename):
     full_path = os.path.join(base, filename)
     if os.path.exists(full_path):
         with open(full_path, "r", encoding="utf-8") as f:
-            return f.read()
+            return f.read(
     return ""
