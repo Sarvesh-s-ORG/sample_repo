58b176d0f0bed3cf5f1952aa52728b26015de7a0
sarrvessh <pvsarvesh29@gmail.com>
2026-05-04 03:22:57 +0530

    fix: correct filename variable in read_log_file function

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def load_template(path):
 
 def read_log_file(filename):
     base = os.path.join(os.getcwd(), "logs")
-    full_path = os.path.join(base, filenam
+    full_path = os.path.join(base, file
     if os.path.exists(full_path):
         with open(full_path, "r", encoding="utf-8") as f:
             return f.read(
